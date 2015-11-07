e24f6d21eb86b07880ba9693fb1c20f1706960d8
Ai Sugimoto <i.sugimoto1112@gmail.com>
2015-11-07 09:37:45 +0900

    modified

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
-echo "hello";
+$n = 10;
+$s = "♪";
+$array = array();
+
+for ($i = 0; $i < $n; $i++) 
+{   
+	//array_pushによる配列要素の追加。
+ 	array_push($array, $s);
+ 	
+} 
+
+ //配列要素を文字列により連結する。
+ $connection = implode($array);
+ 
+ echo $connection;
